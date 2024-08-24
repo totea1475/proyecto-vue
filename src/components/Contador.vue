75311a3cd1df17e84e23838420f2799184c858cd
totea <totea14@gmail.com>
2024-08-24 15:15:10 -0300

    crear contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,26 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+
+<template>
+    <h1>Hola Mundo</h1>
+    <p>{{ contadores }}</p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    </template>
+
+    <script setup>
+    import {ref} from 'vue';
+    const contadores = ref(0);
+    const incrementar = () =>{
+        contadores.value++;
+    }
+    const decrementar = () =>{
+        contadores.value--;
+    }
+</script>
+    
+    
+    <style scoped>
+    p{
+      color: red;
+    }
+    </style>
